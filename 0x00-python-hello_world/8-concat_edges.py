65e0c3be50bc0ef5e956584e106d6e5826dffdf4
Evemustand <evalynemurugami@gmail.com>
2023-01-04 08:42:06 -0800

    task 8

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
-#!/usr/bin/python
-str = "Python is an interpreted, interactactive, object-oriented programming"
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
 str = str[39:66] + str[106:112] + str[:6]
 print(str)
