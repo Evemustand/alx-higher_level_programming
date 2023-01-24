e073c6ea32430d7bb4b96bf7489c4a350af7c055
Evemustand <evalynemurugami@gmail.com>
2023-01-24 05:28:36 -0800

    task 100

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,18 +1,30 @@
 #!/usr/bin/python3
 
+
+
 import sys
+
+
+
+
 
 def safe_print_integer_err(value):
 
     """Prints an integer with "{:d}".format().
 
+
+
     If a ValueError message is caught, a corresponding
 
     message is printed to standard error.
 
+
+
     Args:
 
         value (int): The integer to print.
+
+
 
     Returns:
 
@@ -32,4 +44,4 @@ def safe_print_integer_err(value):
 
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
 
-        return (False);
+        return (False)
