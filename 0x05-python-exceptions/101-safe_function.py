b8d952719eedb3874ea530c5d2de6f096f2c130d
Evemustand <evalynemurugami@gmail.com>
2023-01-24 04:48:03 -0800

    task 101

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
 
-
-
 import sys
-
-
 
 def safe_function(fct, *args):
 
     """Executes a function safely.
+
+
 
     Args:
 
         fct: The function to execute.
 
         args: Arguments for fct.
+
+
 
     Returns:
 
@@ -34,3 +34,4 @@ def safe_function(fct, *args):
 
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
 
+        return (None)
